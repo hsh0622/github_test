c44dcf026aced7ab34ef02e73e3cd7c6d97c641e
한승환 <coderrornotfound@gmail.com>
2025-06-26 09:49:01 +0900

    Create def.py

---
+++ def.py
@@ -0,0 +1,9 @@
+def abs(userInput):
+    if userInput > 0:
+        print(f"절댓값은 {userInput}입니다.")
+    else:
+        print(f"절댓값은 {-userInput}입니다.")
+
+a = int(input())
+abs(a)
+print("Hello World!")
